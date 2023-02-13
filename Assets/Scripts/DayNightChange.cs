73ba3411a50f26f4adc01f11b542b8824e7bfcef
Obsessive Games <Novamancer@gmail.com>
2023-02-13 14:17:58 +0200

    IEnumerator add

--- Assets/Scripts/DayNightChange.cs
+++ Assets/Scripts/DayNightChange.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 [RequireComponent(typeof(Renderer))]
 public class DayNightChange : MonoBehaviour
@@ -9,6 +10,7 @@ public class DayNightChange : MonoBehaviour
     [SerializeField] private Material nightMaterial;
     [SerializeField] private Sprite sunSprite;
     [SerializeField] private Sprite moonSprite;
+    [SerializeField] private float transitionDuration = 1f;
 
     private SpriteRenderer spriteRenderer;
     private bool isDay;
@@ -21,24 +23,38 @@ public class DayNightChange : MonoBehaviour
     private void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        StartCoroutine(DayCycle());
     }
 
-    private void Update()
+    private IEnumerator DayCycle()
     {
-        float currentTime = Time.time;
-        float dayPercentage = currentTime % DayNightCycle.Instance.dayDuration / DayNightCycle.Instance.dayDuration;
+        while (true)
+        {
+            float currentTime = Time.time;
+            float dayPercentage = currentTime % DayNightCycle.Instance.dayDuration / DayNightCycle.Instance.dayDuration;
 
-        if (dayPercentage >= 0.5f)
-        {
-            spriteRenderer.material = nightMaterial;
-            spriteRenderer.sprite = moonSprite;
-            isDay = false;
-        }
-        else
-        {
-            spriteRenderer.material = dayMaterial;
-            spriteRenderer.sprite = sunSprite;
-            isDay = true;
+            Material newMaterial = dayPercentage >= 0.5f ? nightMaterial : dayMaterial;
+            Sprite newSprite = dayPercentage >= 0.5f ? moonSprite : sunSprite;
+            bool newIsDay = dayPercentage < 0.5f;
+
+            if (newMaterial != spriteRenderer.material)
+            {
+                float elapsedTime = 0f;
+                Material currentMaterial = spriteRenderer.material;
+
+                while (elapsedTime < transitionDuration)
+                {
+                    spriteRenderer.material.Lerp(currentMaterial, newMaterial, elapsedTime / transitionDuration);
+                    elapsedTime += Time.deltaTime;
+                    yield return null;
+                }
+
+                spriteRenderer.material = newMaterial;
+                spriteRenderer.sprite = newSprite;
+                isDay = newIsDay;
+            }
+
+            yield return null;
         }
     }
 
